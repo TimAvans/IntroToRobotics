df72af7823bdf1e93c72ab434def75c6a14063e8
Tim <38493728+TimAvans@users.noreply.github.com>
2024-04-12 16:47:50 +0200

    Progress day 1

--- Line_following_task/lineMaze.py
+++ Line_following_task/lineMaze.py
@@ -1,7 +1,8 @@
 import sim
 import matplotlib.pyplot as plt
 from mindstorms import Motor, Direction, ColorSensor
-
+import numpy as np
+import datetime
 sim.simxFinish(-1)
 clientID = sim.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
 
@@ -40,19 +41,47 @@ def follow_line(color_sensor, left_motor, right_motor):
     """
     A very simple line follower that should be improved.
     """
+
     color_sensor.image = color_sensor._get_image_sensor()
-
+    
     reflection = color_sensor.reflection()
-    print(reflection)
+    old_reflection = 0
+    # print(reflection)
     threshold = 40  # Midpoint between black and white
+    left, right = pid_controller(reflection, old_reflection)
+    print("LEFT: " + left.__str__() + " RIGHT: " + right.__str__())
+    left_motor.run(speed=left)
+    right_motor.run(speed=right)
+    old_reflection = reflection
 
-    if reflection < threshold:
-        left_motor.run(speed=-1)
-        right_motor.run(speed=4)
-    else:
-        left_motor.run(speed=4)
-        right_motor.run(speed=-1)
+    # if reflection < threshold:
+    #     left_motor.run(speed=-1)
+    #     right_motor.run(speed=4)
+    # else:
+    #     left_motor.run(speed=4)
+    #     right_motor.run(speed=-1)
 
+CONSTANT_ERROR = 7
+CONSTANT_D_ERROR = 0.0005
+LEFT_THRESHOLD = 40
+RIGHT_THRESHOLD = 60
+
+def pid_controller(error, old_error):
+    # Proportional
+    left_motor_speed = CONSTANT_ERROR * ((LEFT_THRESHOLD - error) / 100) + 1
+    right_motor_speed = CONSTANT_ERROR * ((error - RIGHT_THRESHOLD) / 100) + 1
+
+    # Derivative
+    derivative = error - old_error
+    left_motor_speed -= derivative * CONSTANT_D_ERROR
+    right_motor_speed += derivative * CONSTANT_D_ERROR
+
+    # Turn off motor in case value is negative
+    if left_motor_speed < 0:
+        left_motor_speed = -1
+    if right_motor_speed < 0:
+        right_motor_speed = -1
+    return (left_motor_speed, right_motor_speed)
 
 # MAIN CONTROL LOOP
 if clientID != -1:
@@ -65,9 +94,7 @@ if clientID != -1:
 
     while True:
         # End connection
-
-        left_motor.run(speed=5)
-        right_motor.run(speed=5)
+        follow_line(color_sensor, left_motor, right_motor)
 
 else:
     print('Failed connecting to remote API server')
